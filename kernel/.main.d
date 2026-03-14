8772e00375eead12a2e65192eac2d3b98bced2f4
Otinpan <hasesyou7531@gmail.com>
2026-03-14 20:23:06 +0900

    refactoring

---
+++ kernel/.main.d
@@ -0,0 +1,7 @@
+main.o: main.cpp \
+  /home/hase/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/hase/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/hase/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/hase/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/hase/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  frame_buffer_config.hpp graphics.hpp font.hpp
